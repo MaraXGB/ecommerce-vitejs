0a35ab44c23917a46af1b041de5f30f121412c9f
Edmara Guerrero <edxilcoil@gmail.com>
2025-01-09 16:49:01 -0500

    creacion de componente layout

--- src/Pages/MyOrders/index.jsx
+++ src/Pages/MyOrders/index.jsx
@@ -1,9 +1,9 @@
-
+import Layout from "../../Components/Layout"
 function MyOrders() {
     return (
-        <div className='bg-red-100'>
+        <Layout>
           MyOrders
-        </div>
+        </Layout>
     )
   }
   
