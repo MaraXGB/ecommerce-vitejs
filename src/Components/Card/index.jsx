f693273a1ac63bd4230595e2ba16fbbbc764b665
Edmara Guerrero <edxilcoil@gmail.com>
2025-01-14 17:34:44 -0500

    componente card, peticion de api products, agregar datos a la card y colocar en grid e el home

---
+++ src/Components/Card/index.jsx
@@ -0,0 +1,18 @@
+const Card =(data)=>{
+    return(
+        <div className="bg-white cursor-pointer w-56 h-70  rounded-lg">
+            <figure className="relative mb-2 w-full h-4/5">
+                <span className="absolute bottom-2 left-2 bg-white/60 rounded-lg text-black text-sm px-3 py-2">{data.data.category.name}</span>
+                <img className="w-full h-full object-cover rounded-lg" src={data.data.images[0]} alt={data.data.title} />
+                <div className="absolute -bottom-4 right-4 flex justify-center items-center border-lg bg-indigo-950 shadow-lg text-white w-10 h-10 rounded-full text-lg font-bold">
+                +
+                </div>
+            </figure>
+            <p className="flex justify-between mt-5">
+                <span className="text-sm font-light">{data.data.title}</span>
+                <span className="text-lg font-medium">${data.data.price}</span>
+            </p>
+        </div>
+    );
+}
+export default Card;
--- src/Components/Layout/index.jsx
+++ src/Components/Layout/index.jsx
@@ -1,6 +1,6 @@
 const Layout =({ children }) => {
     return(
-        <div className="flex flex-col mt-20">
+        <div className="flex flex-col mt-20 p-20">
             {children}
         </div>
     )
