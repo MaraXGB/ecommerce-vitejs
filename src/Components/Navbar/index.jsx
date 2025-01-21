ca691cf5583927284cdb73d17dc967d49c3d1468
MaraXGB <edxilcoil@gmail.com>
2025-01-21 10:46:18 -0500

    creacion de contexto para incrementar los productos en el carrito de compras

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,6 +1,10 @@
+
+import { useContext } from "react"
+import { ShoppingCartContext } from "../../Context"
 import { NavLink } from "react-router"
 const Navbar = () => {
     const activeStyle =  "underline underline-offset-4"
+    const context = useContext(ShoppingCartContext)
 
     return(
         <nav className="flex justify-between items-center fixed z-10 top-0 w-full py-5 px-8 text-sm font-light">
@@ -88,7 +92,7 @@ const Navbar = () => {
                     </NavLink>
                 </li>
                 <li>
-                    🛒0
+                    🛒{context.count}
                 </li>
             </ul>
         </nav>
