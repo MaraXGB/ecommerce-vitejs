bce7d47114a86abab3d44cb01eddd99c8442294a
MaraXGB <edxilcoil@gmail.com>
2025-03-19 09:23:37 -0500

    funciones iniciales para checkout del carrito de compras

--- src/Components/CheckoutSideMenu/index.jsx
+++ src/Components/CheckoutSideMenu/index.jsx
@@ -10,6 +10,17 @@ const CheckoutSideMenu = () =>{
         const filteredProducts = context.cartProducts.filter(product => product.id !== id );
         context.setCartProducts(filteredProducts);
     }
+const handleCheckout = () => {
+    const orderToAdd = {
+        date: "2021-09-01",
+        products: context.cartProducts,
+        totalProducts: context.cartProducts.length,
+        total: totalPrice(context.cartProducts)
+    }
+    context.setOrder([...context.order,orderToAdd]);
+    context.setCartProducts([]);
+    // context.closeCheckoutSideMenu();
+}
     //if(!context.productDetail || !context.productDetail.images) return null;
     return(
         <aside className={`${context.isCheckoutSideMenuOpen? 'flex': 'hidden'} checkout-side-menu flex-col fixed right-0 border border-gray rounded bg-white shadow-lg`}>
@@ -20,7 +31,7 @@ const CheckoutSideMenu = () =>{
                         <XMarkIcon className="size-6 text-black-500" />
                 </div>
             </div>
-            <div className='px-6 h-full overflow-y-scroll'>
+            <div className='px-6 h-full overflow-y-scroll flex-1 mb-6'>
                 {
                     context.cartProducts.map(product => (
                         <OrderCard key={product.id} 
@@ -38,6 +49,7 @@ const CheckoutSideMenu = () =>{
                 <span className='font-ligth'>Total: </span>
                 <span className='font-medium font-2xl'>${totalPrice(context.cartProducts)}</span>
                </p>
+               <button className='w-full bg-indigo-950 text-white rounded mt-2 mb-4 py-2' onClick={()=> handleCheckout()}>Checkout</button>
             </div>
         </aside>
     );
